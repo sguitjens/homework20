98f12c5366523f1abe98008b2e9ec97fb6d528be
Sydney <sguitjens@gmail.com>
2020-03-24 20:16:04 -0700

    add rel=noopener for security reasons with target=_blank

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,7 +6,7 @@ function Project(props) { // imgSrc, altText, gitHubURL
     // <div className="col-md-6 position-relative gallery-img">
     //   {/* <img src={props.imgSrc} alt={props.altText} class="img-fluid figure-image gallery-img"> */}
     //   <div className="col-12 img-caption no-gutter position-absolute">
-    //     <a href={props.gitHubURL} alt={props.altText} target="_blank" className="row btn btn-secondary btn-lg btn-block active img-caption no-gutter" role="button" id="img-btn">theLab</a>
+    //     <a href={props.gitHubURL} alt={props.altText} target="_blank" rel="noopener" className="row btn btn-secondary btn-lg btn-block active img-caption no-gutter" role="button" id="img-btn">theLab</a>
     //   </div>
     // </div>
     // <div className="row">
@@ -17,7 +17,7 @@ function Project(props) { // imgSrc, altText, gitHubURL
       <img className="card-img-top" src={props.imgSrc} alt={props.altText}/>
       <div className="card-body d-flex flex-column">
         {/* <p className="card-text">{props.label}</p> */}
-        <a href={props.gitHubURL} alt={props.altText} target="_blank" className="btn btn-info btn-lg btn-block active img-caption mt-auto" role="button" id="img-btn">{props.label}</a>
+        <a href={props.gitHubURL} alt={props.altText} target="_blank" rel="noopener" className="btn btn-info btn-lg btn-block active img-caption mt-auto" role="button" id="img-btn">{props.label}</a>
       </div>
     </div>
 
